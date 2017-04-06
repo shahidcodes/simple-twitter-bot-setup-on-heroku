231ae151025bda9ca71f04b5715dd278385f2c82
shahidkh4n <shahidkamal08@gmail.com>
2017-04-06 06:44:58 +0530

    first initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# simple-twitter-bot-setup-on-heroku
-Bot setup example repository on heroku
+# Simple Twitter Bot Setup On Heroku
+
+# Pre-requisite
+* Python Installed Obviously
+* VirtualEnv Installed (pip install virtualenv)
+* You must know how to deploy app on heroku [Read here](https://devcenter.heroku.com/articles/getting-started-with-python)
+
+# Get Started
+We're going to use heroku's worker dyno to acomplish this task. Although you can use job scheduling to run your bot at certain time than keep running it all time.
+
+* **Clone this Repo**
+* run `virtualenv twibot --no-site-packages` and activate it.
+* Make changes to `main.py` because this is your Bot main entry file.
+* see `main_example.py` for reference.
+* When all done means when you've your working bot locally. Run `pip freeze requirements.txt` in the bot directory.
+* And finally deploy and scale the worker dyno or run it.
+# Quick Walkthrough
+
+## Procfile
+This is main file which tells Heroku, which dyno to use. We're using `worker` dyno. [Learn more about dyno](https://devcenter.heroku.com/articles/dynos).
+So `worker: python main.py` tells heroku to run worker dyno and run `python main.py` command on that.
+ 
+## Requirements (requirements.txt)
+This is file created by pip when we run `pip freeze requirements.txt`. This is important because heroku will look for this file to install module dependency of your bot.
+
+## Runtime (runtime.txt)
+If you're using python 2.7 then change runtime in this file. Available runtimes are listed [here](https://devcenter.heroku.com/articles/python-runtimes)
+
+# Me
+### Twitter: [@sh4hidkh4n](https://twitter.com/sh4hidkh4n)
---
+++ main_example.py
@@ -0,0 +1,102 @@
+try:
+  import tweepy
+  import logging
+  from time import sleep
+  import time
+  import os
+except BaseException as e:
+  print(e)
+
+logging.basicConfig()
+logger = logging.getLogger("BOT")
+logger.setLevel(logging.DEBUG)
+
+
+consumer_key = "consumer_key"
+consumer_secret = "secret"
+key = "token"
+secret = "token_secret"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+api = tweepy.API(auth)
+print("Welcome {}".format(api.me().screen_name) )
+BLACKLIST_WORDS = [
+  'RT'
+]
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api_handle):
+      self.found_retweets = 0
+      self.api = api_handle
+      self.retweeted = 0
+      self.retweet_limit = 2400
+    
+    def retweet(self, tweet_id):
+      rt = self.api.retweet(tweet_id)
+      if rt:
+        return True
+      return False
+    def plusOneRT(self):
+      currentDay = time.strftime("%d%b") #04Apr
+      limitsInFile = None 
+      with open(currentDay, 'r') as f:
+        limitsInFile = f.read()
+      with open(currentDay, 'w') as f:
+        count = str(int(limitsInFile) - 1)
+        f.write(count)
+    def inLimits(self):
+      logger.info("Checking Limits")
+      currentDay = time.strftime("%d%b") #04Apr
+      if os.path.exists(currentDay):
+        logger.info("Information file found")
+        with open(currentDay, 'r') as f:
+          logger.debug(int(f.read()) == 0)
+          return not int(f.read()) == 0
+      else:
+        logger.info("Info file not found. Creating One.")
+        with open(currentDay,'w') as f:
+          f.write( str( self.retweet_limit ) )
+          logger.info("Initial Info Saved!")
+          return True
+
+    def on_status(self, status):
+      if not self.inLimits():
+        logger.info("All Done For Today! Limits are over!")
+        return
+      else:
+        try:
+            logger.info("All Ok! Proceeding..")
+            if status.lang != 'en':
+                logger.warning("Foriegn Language Detected :{}".format(status.lang))
+                return
+            status_text = status.text
+            if "RT" in status_text:
+              logger.warning("Skipped Retweet")
+              self.found_retweets += 1
+              return
+            publish = None
+            for w in BLACKLIST_WORDS:
+              if w.lower() in status_text.lower():
+                logger.warning("Skipped For Word: {}".format(w))
+                publish = False
+                return
+            if publish:
+              tweet_id = status.id
+              try:
+                sleep(2)
+                self.retweet(tweet_id)  
+                self.plusOneRT()
+                logger.info("Retweeted {}".format(status_text))
+              except Exception as e :
+                logger.warning(e)
+        except Exception as e:
+            logger.warning(e)
+    def on_error(self, status_code):
+        print("Some Error")
+        logger.error(status_code)
+               
+myStreamListener = MyStreamListener(api)
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+
+myStream.filter(track=['#100DaysOfCode'], languages=['en'], async=True)
